940af30e98258f582aeb237eaaff83c84a86e532
Yeong-si <dtd1110@gmail.com>
2025-10-11 15:48:21 +0900

    feat : input 컴포넌트 onChangeText props 추가

--- src/components/common/Input.tsx
+++ src/components/common/Input.tsx
@@ -7,12 +7,13 @@ interface InputProps extends TextInputProps {
   className?: string;
 }
 
-const Input = ({ label, className, ...rest }: InputProps) => {
+const Input = ({ label, className, onChangeText ,...rest }: InputProps) => {
   return (
     <View className={`${className} flex flex-col items-start gap-1`}>
       <Text className="text-sm font-medium mb-2">{label}</Text>
       <TextInput
         className="w-full bg-white border border-input-gray rounded-lg p-4 text-base text-black"
+        onChangeText={onChangeText}
         {...rest}
       />
     </View>
