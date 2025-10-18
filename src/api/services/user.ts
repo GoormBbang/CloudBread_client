c88497c4d8453d816da2566cae83bf76f4984d69
Yeong-si <dtd1110@gmail.com>
2025-10-19 00:22:11 +0900

    feat : 개인정보 수정 구현 - 개인 정보 수정 UI - 개인 정보 전체 조회 api - 개인 정보 수정 api

--- src/api/services/user.ts
+++ src/api/services/user.ts
@@ -1,10 +1,28 @@
-import apiClient  from "../client";
+import apiClient from "../client";
 import { ApiResponse } from "../types/common";
-import { MetadataResponse } from "../types/user";
+import { MetadataResponse, UpdateUserPayload } from "../types/user";
 
 //건강정보 메타데이터가져오기
 
 export const getMetadata = async (): Promise<ApiResponse<MetadataResponse>> => {
-  const response = await apiClient.get<ApiResponse<MetadataResponse>>('/metadata');
+  const response =
+    await apiClient.get<ApiResponse<MetadataResponse>>("/metadata");
+  return response.data;
+};
+
+// 현재 로그인된 사용자의 상세 정보를 조회
+export const getMyProfile = async (): Promise<ApiResponse<any>> => {
+  const response = await apiClient.get<ApiResponse<any>>("/users/me");
+  return response.data;
+};
+
+//사용자의 수정된 정보 저장
+export const updateMyProfile = async (
+  payload: UpdateUserPayload
+): Promise<ApiResponse<{ id: number }>> => {
+  const response = await apiClient.put<ApiResponse<{ id: number }>>(
+    "/users/me",
+    payload
+  );
   return response.data;
 };
